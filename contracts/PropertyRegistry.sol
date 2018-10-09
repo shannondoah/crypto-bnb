10d7fb3a726bcbeab38bb50608da53f899df45d6
Shannon <shannon.mcdonald46@gmail.com>
2018-10-08 20:52:06 -0700

    Add more logic to registry

--- contracts/PropertyRegistry.sol
+++ contracts/PropertyRegistry.sol
@@ -5,11 +5,19 @@ import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Basic.sol';
 contract PropertyRegistry {
   ERC721Basic public property;
   mapping(uint256 => Data) public stayData;
+  mapping(uint256 => Request) public requests;
 
   struct Data {
     uint256 price;
     uint256 stays;
     address occupant;
+  }
+
+  struct Request {
+    uint256 checkIn;
+    uint256 checkOut;
+    address guest;
+    bool approved;
   }
 
   constructor(address _property) public {
@@ -21,7 +29,44 @@ contract PropertyRegistry {
     _;
   }
 
+  modifier requestApproved(uint256 _tokenId) {
+    require(requests[_tokenId].approved && requests[_tokenId].guest == msg.sender, "Request not approved for this sender");
+    _;
+  }
+
   function registerProperty(uint256 _tokenId, uint256 _price) external onlyOwner(_tokenId) {
     stayData[_tokenId] = Data(_price, 0, address(0));
+  }
+
+  function request(uint256 _tokenId, uint256 _checkIn, uint256 _checkOut) external {
+    require(stayData[_tokenId].price > 0, "Property must be registered");
+    require(requests[_tokenId].guest == address(0), "Property must not have any requests in progress");
+    require(_checkIn > now, "Check-in must be in the future");
+    require(_checkOut > _checkIn, "Check out time must be after check-in");
+
+    requests[_tokenId] = Request(_checkIn, _checkOut, msg.sender, false);
+    // should be able to put in a new request if an existing request is idle (check in time has passed)
+  }
+
+  function approveRequest(uint256 _tokenId, bool _approved) external onlyOwner(_tokenId) {
+    if(_approved) {
+      requests[_tokenId].approved = true;
+    } else {
+      delete requests[_tokenId];
+    }
+    // should not be able to approve a check-in request for which the start time is now in the past
+  }
+
+  function checkIn(uint256 _tokenId) external requestApproved(_tokenId) {
+    require(requests[_tokenId].checkIn <= now, "Check-in has not yet begun");
+    stayData[_tokenId].stays++;
+    stayData[_tokenId].occupant = msg.sender;
+  }
+
+  function checkOut(uint256 _tokenId) external {
+    require(stayData[_tokenId].occupant == msg.sender, "Sender must be current occupant");
+    // record if checkout is late, penalize occupant
+    delete requests[_tokenId];
+    stayData[_tokenId].occupant = address(0);
   }
 }
--- test/property_registry.js
+++ test/property_registry.js
@@ -4,7 +4,7 @@ const Property = artifacts.require("./Property.sol");
 contract('PropertyRegistry', (accounts) => {
 
     let property, registry;
-    const alice = accounts[0], bob = accounts[1];
+    const alice = accounts[0], bob = accounts[1], eve = accounts[3];
 
     it("should be deployed, PropertyRegistry", async () => {
         var registry = await PropertyRegistry.deployed();
@@ -14,11 +14,24 @@ contract('PropertyRegistry', (accounts) => {
     const newRegistry = async () => {
         property = await Property.new();
         registry = await PropertyRegistry.new(property.address);
+        await property.createProperty();
     }
+
+    const requestBooking = async () => {
+        await registry.registerProperty(1, 100);
+        await registry.request(1, nextWeek, weekAfterThat, {from: bob});
+    }
+
+    const sevenDaysAfter = (time) => {
+        return new Date(time + 7 * 24 * 60 * 60 * 1000).getTime();
+    }
+
+    const currentTime = Date.now();
+    const nextWeek = sevenDaysAfter(currentTime);
+    const weekAfterThat = sevenDaysAfter(nextWeek);
 
     it("should allow alice to register her property", async () => {
         await newRegistry();
-        await property.createProperty();
         try {
             await registry.registerProperty(1, 100);
             assert(true, "Alice could register her property");
@@ -29,7 +42,6 @@ contract('PropertyRegistry', (accounts) => {
 
     it("should not allow bob to register alice's property", async () => {
         await newRegistry();
-        await property.createProperty();
         try {
             await registry.registerProperty(1, 100, {from: bob});
             assert(false, "Bob could register Alice's property");
@@ -37,5 +49,138 @@ contract('PropertyRegistry', (accounts) => {
             assert(true, "Bob could not register Alice's property");
         }
     });
+
+    it("should not allow anyone to make a booking request for an unregistered property", async () => {
+        await newRegistry();
+        try {
+            await registry.request(1, nextWeek, weekAfterThat, {from: bob})
+            assert(false, "Bob could submit a booking request for an unregistered property.");
+        } catch (e) {
+            assert(true, "Bob could not submit a booking request for an unregistered property.");
+        }
+    });
+
+    it("should allow anyone to make a booking request with valid dates", async () => {
+        await newRegistry();
+        await registry.registerProperty(1, 100);
+        try {
+            await registry.request(1, nextWeek, weekAfterThat, {from: bob})
+            assert(true, "Bob could submit a booking request.");
+        } catch (e) {
+            assert(false, "Bob could not submit a booking request.");
+        }
+    });
+
+    it("should not allow anyone to make a booking request if check in is before the block time", async () => {
+        await newRegistry();
+        await registry.registerProperty(1, 100);
+        try {
+            await registry.request(1, currentTime, nextWeek, {from: bob})
+            assert(false, "Bob could submit a booking request.");
+        } catch (e) {
+            assert(true, "Bob could not submit a booking request.");
+        }
+    });
+
+    it("should not allow anyone to make a booking request if check out is before check in", async () => {
+        await newRegistry();
+        await registry.registerProperty(1, 100);
+        try {
+            await registry.request(1, nextWeek, currentTime, {from: bob})
+            assert(false, "Bob could submit a booking request.");
+        } catch (e) {
+            assert(true, "Bob could not submit a booking request.");
+        }
+    });
+
+    it("should not allow anyone to make a booking request if there is already a request in progress", async () => {
+        await newRegistry();
+        await requestBooking();
+        try {
+            await registry.request(1, nextWeek, currentTime, {from: eve});
+            assert(false, "Eve could submit a booking request.");
+        } catch (e) {
+            assert(true, "Eve could not submit a booking request.");
+        }
+    });
+
+    it("should allow alice to approve bob's request", async () => {
+        await newRegistry();
+        await requestBooking();
+        await registry.approveRequest(1, true);
+        const request = await registry.requests.call(1);
+        assert(request[3] === true, "Alice could approve bob's request.");
+    });
+
+    it("should allow alice to reject bob's request", async () => {
+        await newRegistry();
+        await requestBooking();
+        await registry.approveRequest(1, false);
+        const request = await registry.requests.call(1);
+        assert(request[0].toString() === "0", "Alice rejected bob's request.");
+    });
+
+    it("should allow bob to check in if check-in time has begun and the request is approved", async () => {
+        await newRegistry();
+        await requestBooking();
+        await registry.approveRequest(1, true);
+        // move time forward??
+        try {
+            await registry.checkIn(1, {from: bob});
+            assert(true, "Bob could check in");
+        } catch(e) {
+            assert(false, "Bob could not check in");
+        }
+    });
+
+    it("should not allow bob to check in if check-in time has not begun", async () => {
+        await newRegistry();
+        await requestBooking();
+        await registry.approveRequest(1, true);
+        try {
+            await registry.checkIn(1, {from: bob});
+            assert(false, "Bob could check in");
+        } catch(e) {
+            assert(true, "Bob could not check in");
+        }
+    });
+
+    it("should not allow bob to check in if the request is not approved", async () => {
+        await newRegistry();
+        await requestBooking();
+        await registry.approveRequest(1, false);
+        try {
+            await registry.checkIn(1, {from: bob});
+            assert(false, "Bob could check in");
+        } catch(e) {
+            assert(true, "Bob could not check in");
+        }
+    });
+
+    it("should not allow eve to check in at any time", async () => {
+        await newRegistry();
+        await requestBooking();
+        await registry.approveRequest(1, true);
+        // move the clock forward??
+        try {
+            await registry.checkIn(1, {from: eve});
+            assert(false, "Eve could check in");
+        } catch(e) {
+            assert(true, "Eve could not check in");
+        }
+    });
+
+    // it("should allow bob to check out", async () => {
+
+    // });
+
+    // it("should not allow eve to check out", async () => {
+
+    // });
+
+    // it("should allow eve to submit a request after bob has checked out", async () => {
+
+    // });
+
 });
 
