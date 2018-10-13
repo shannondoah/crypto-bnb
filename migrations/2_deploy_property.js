8c6cd1ef36de08ea6ab1ae97ba06e3307e237b08
Shannon <shannon.mcdonald46@gmail.com>
2018-10-13 14:09:51 -0700

    Enter callback hell for dependent deploys

--- migrations/2_deploy_property.js
+++ migrations/2_deploy_property.js
@@ -2,8 +2,10 @@ var Property = artifacts.require("./Property.sol");
 var PropertyRegistry = artifacts.require("./PropertyRegistry.sol");
 var PropertyToken = artifacts.require("./PropertyToken.sol");
 
-module.exports = function(deployer) {
-    deployer.deploy(Property);
-    deployer.deploy(PropertyToken);
-    deployer.deploy(PropertyRegistry, Property.address, 0);
+module.exports = async function(deployer) {
+    deployer.deploy(Property).then( () => {
+        return deployer.deploy(PropertyToken).then( () => {
+            return deployer.deploy(PropertyRegistry, Property.address, PropertyToken.address);
+        });
+    });
 };
