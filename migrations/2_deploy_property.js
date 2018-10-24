f31568c13c0bfc4e8c0f29976ba23192b8aa835b
Shannon <shannon.mcdonald46@gmail.com>
2018-10-23 19:51:08 -0700

    Fix up dependent deploys

--- migrations/2_deploy_property.js
+++ migrations/2_deploy_property.js
@@ -1,11 +1,9 @@
 var Property = artifacts.require("./Property.sol");
-var PropertyRegistry = artifacts.require("./PropertyRegistry.sol");
 var PropertyToken = artifacts.require("./PropertyToken.sol");
+var PropertyRegistry = artifacts.require("./PropertyRegistry.sol");
 
-module.exports = async function(deployer) {
-    deployer.deploy(Property).then( () => {
-        return deployer.deploy(PropertyToken).then( () => {
-            return deployer.deploy(PropertyRegistry, Property.address, PropertyToken.address);
-        });
-    });
+module.exports = async (deployer) => {
+    await deployer.deploy(Property);
+    await deployer.deploy(PropertyToken);
+    await deployer.deploy(PropertyRegistry, Property.address, PropertyToken.address);
 };
